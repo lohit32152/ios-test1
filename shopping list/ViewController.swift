788e9c56f2329a2f062debb4a3edb421402f4a33
Lohit Mahay <lohit32152@gmail.com>
2019-10-16 18:53:24 -0400

    Initial Commit

---
+++ shopping list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopping list
+//
+//  Created by Lohit Mahay on 2019-10-16.
+//  Copyright © 2019 Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
